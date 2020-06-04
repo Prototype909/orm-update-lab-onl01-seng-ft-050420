29014810e8f3c000c61e9ab116152fa6124a7027
Steve Anderson <prototype909@msn.com>
2020-06-04 02:33:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,8 +55,6 @@ class Student
     sql = "SELECT * FROM students WHERE name = ?"
       result = DB[:conn].execute(sql, name)[0]
       Student.new(result[0], result[1], result[2])
-    end
-
   end
 
   def update
